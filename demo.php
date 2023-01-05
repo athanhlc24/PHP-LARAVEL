8bb26e202ee20bf3532c14f53228e6a6f9462a3b
Administrator <tranvanthanh060699@gmail.com>
2023-01-05 18:43:55 +0700

    up

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+// viet code PHP tai day
+echo "hello";
+$x = 10;
+$x = "hello";// du lieu dong => kieu du lieu k chat che
+$arr = [];// khai bao mang
+$arr[0] = 15;
+$arr[1] = 20;
+$arr[2] = 25;
+
+for ($i=0;$i<count($arr);$i++){
+
+}
